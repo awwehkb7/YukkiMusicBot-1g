466e59b5906b2bca64ee0cf7924b128ff885565b
MOMf <115490711+awwehkb7@users.noreply.github.com>
2022-10-10 22:08:56 +0300

    Add files via upload

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -38,9 +38,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001547633469
+                                and chat_id != -1001611511207
+                                and chat_id != -1001752899944
                             ):
                                 if left == 20:
                                     continue
